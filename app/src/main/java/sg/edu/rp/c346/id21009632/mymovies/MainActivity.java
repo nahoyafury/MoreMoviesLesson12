55e96f29bde1883932fd3a71f5af082f30419571
21009632 <21009632@myrp.edu.sg>
2022-07-27 14:28:36 +0800

    Added codes for spinner

--- app/src/main/java/sg/edu/rp/c346/id21009632/mymovies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21009632/mymovies/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -49,6 +50,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        spinRating.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+
+
+        });
     }
 }
