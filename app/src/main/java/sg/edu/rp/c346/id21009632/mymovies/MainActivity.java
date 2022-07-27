f2456ec8570d0d4087d089c06323b1601588d22a
fikry <21040742@myrp.edu.sg>
2022-07-27 14:06:12 +0800

    Added stuff in the activity_main.xml

--- app/src/main/java/sg/edu/rp/c346/id21009632/mymovies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21009632/mymovies/MainActivity.java
@@ -3,12 +3,51 @@ package sg.edu.rp.c346.id21009632.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etTitle, etYear, etGenre;
+    Button btnShow, btnInsert;
+    Spinner spinRating;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etTitle = findViewById(R.id.etTitle);
+        etGenre = findViewById(R.id.etGenre);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnShow = findViewById(R.id.btnShow);
+        spinRating = findViewById(R.id.spinRating);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String movieTitle = etTitle.getText().toString();
+                String genre = etGenre.getText().toString();
+                int year = Integer.parseInt(etYear.getText().toString());
+
+
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long inserted_id = dbh.insertSong(songTitle, singer, year, stars);
+
+                if (inserted_id != -1) {
+                    Toast.makeText(MainActivity.this, "Added " + movieTitle + " to the movie list successfully!", Toast.LENGTH_LONG).show();
+
+                    etTitle.setText("");
+                    etGenre.setText("");
+                    etYear.setText("");
+                    rgStars.clearCheck();
+                }
+            }
+        });
+
+
     }
 }
