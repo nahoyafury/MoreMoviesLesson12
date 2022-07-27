aabebd8d3ebd6982a313b46e488b780ad2443d59
21009632 <21009632@myrp.edu.sg>
2022-07-27 13:01:37 +0800

    Added codes in DBHelper

--- app/src/main/java/sg/edu/rp/c346/id21009632/mymovies/DBHelper.java
+++ app/src/main/java/sg/edu/rp/c346/id21009632/mymovies/DBHelper.java
@@ -1,4 +1,43 @@
 package sg.edu.rp.c346.id21009632.mymovies;
 
-public class DBHelper {
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_MOVIE = "movies";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_GENRE = "genre";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_RATING = "rating";
+
+    public DBHelper(Context context) {super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_MOVIE + "("
+                + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT, "
+                + COLUMN_GENRE + " TEXT, "
+                + COLUMN_YEAR + " INTEGER, "
+                + COLUMN_RATING + " TEXT ) ";
+        db.execSQL(createNoteTableSql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MOVIE);
+        onCreate(db);
+    }
+
+
 }
+
